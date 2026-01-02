30f011728eda6a2e8008a94ef6df29cd674653be
sowmya172 <sowmyasudhagani5@gmail.com>
2026-01-02 09:27:11 +0530

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,14 @@
+class Matrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 2, 3 },
+                { 4, 5, 6 }
+        };
+        for (int[] matrix1 : matrix) {
+            for (int j = 0; j < matrix1.length; j++) {
+                System.out.print(matrix1[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
