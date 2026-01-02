4113d9b713d774aa918d194999318d4c7993c1cb
sowmya172 <sowmyasudhagani5@gmail.com>
2026-01-02 09:26:49 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,8 @@
+public class StringMethods {
+    public static void main(String[] args, String string) {
+        String s1 = "JAVA";
+        String s2 = "JAVA";
+        System.out.println(s1=s2);
+        System.out.println(s1.equals(s2)); 
+    }
+}
