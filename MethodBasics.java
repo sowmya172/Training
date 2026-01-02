475443d7b1a235ebe07cb7e5cfb4f1a8309a7f1f
sowmya172 <sowmyasudhagani5@gmail.com>
2026-01-02 09:30:32 +0530

    Add files via upload

---
+++ MethodBasics.java
@@ -0,0 +1,17 @@
+
+
+public class MethodBasics {
+
+  
+    static int sum(int... numbers) {
+        int total = 0;
+        for (int n : numbers) {
+            total = total + n;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(1, 2));
+    }
+}
